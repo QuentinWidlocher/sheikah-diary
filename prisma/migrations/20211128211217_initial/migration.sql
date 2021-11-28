6cdd349f5d5e3e4270b4111300a27c7246178a1b
Quentin Widlocher <quentin.widlocher@erudo.fr>
2021-11-28 22:17:09 +0100

    🗃️ Change db to postgres

--- prisma/migrations/20211128211217_initial/migration.sql
+++ prisma/migrations/20211128211217_initial/migration.sql
@@ -1,21 +1,24 @@
 -- CreateTable
 CREATE TABLE "Entry" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" TEXT NOT NULL,
     "slug" TEXT NOT NULL,
     "title" TEXT NOT NULL,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" DATETIME NOT NULL,
-    "content" TEXT NOT NULL
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "content" TEXT NOT NULL,
+
+    CONSTRAINT "Entry_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "Picture" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" TEXT NOT NULL,
     "file" TEXT NOT NULL,
     "preview" TEXT NOT NULL,
     "thumbnail" TEXT NOT NULL,
     "entryId" TEXT NOT NULL,
-    CONSTRAINT "Picture_entryId_fkey" FOREIGN KEY ("entryId") REFERENCES "Entry" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+
+    CONSTRAINT "Picture_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
@@ -29,3 +32,6 @@ CREATE UNIQUE INDEX "Picture_preview_key" ON "Picture"("preview");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Picture_thumbnail_key" ON "Picture"("thumbnail");
+
+-- AddForeignKey
+ALTER TABLE "Picture" ADD CONSTRAINT "Picture_entryId_fkey" FOREIGN KEY ("entryId") REFERENCES "Entry"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
